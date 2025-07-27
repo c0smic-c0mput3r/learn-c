cdd074fe5faac11381221586c5a82850a6009127
c0smic-c0mput3r <mothersiyaa@gmail.com>
2025-07-27 20:19:04 +0530

    cosmic hello launched 4 the rosette nebula

--- hello_cosmos/hello_rn.c
+++ hello_cosmos/hello_rn.c
@@ -2,10 +2,10 @@
 #include <time.h>
 
 int main(){
-    time_t now = time(NULL);
-    char *readable = ctime(&now);
-    printf("hello!, rosette nebula\n");
-    printf("cosmic hello succesfully launched at %slocal earth time\n",readable);
+    time_t now = time(NULL); //this gives no of seconds since unix epoch (jan 1, 1970, 00:00:00 UTC)
+    char *readable = ctime(&now); // converts now into human readable local time (string format ends with \n)
+    printf("hello!, rosette nebula\n"); 
+    printf("cosmic hello succesfully launched at %slocal earth time\n",readable); 
     printf("type: emission nebula\n");
     printf("located: constellation monoceros(the unicorn)\n");
     printf("dist from earth: 5,219 light years\n");
