e9ffa01620d315193ba418fe737587b7add9f660
c0smic-c0mput3r <mothersiyaa@gmail.com>
2025-08-29 12:01:49 +0530

    comments, types, best practices

--- concepts_c/basics/comments.c
+++ concepts_c/basics/comments.c
@@ -6,10 +6,22 @@
 /* this is a multi line comment in c,
  it can span across lines, useful for
  long explanation */
-
+ 
 int main(){
+
+    int edge = 4; // edge length of cube 
+    int fa = 4* /* face area of cube with side */ 4;
+     /* volume of cube */ int vol = 4 * 4 * 4 ;
+    printf("cube of edge length: %d units, has\nface area: %d sq units\nvolume: %d cubic units\n", edge, fa, vol);
+
     printf("executed succesfully\n");
     return 0;
 }
+
+/* comments can be written b4,after,in between the instruction statements beacuse during compilation they are ignored by the compiler;
+   however to avoid redundancy use them stragetically to reason behind complex logic or to comment out lines of code for testing; */
+
+ 
+
 
  
